fe869aa75bb7073f3a7e34ead3c488443af7ee1e
Vinesh <linuxnemesis27@gmail.com>
2023-07-23 15:02:46 +0530

    Final Commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -48,7 +48,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {
         var list = passedList
-        while(list.contains('X') || list.contains('/') || list.contains('%'))
+        while(list.contains('X') || list.contains(('/')))
             list = calcTimesDiv(list)
 
         return list
@@ -88,10 +88,6 @@ class MainActivity : AppCompatActivity() {
                     }
                     '/'->{
                         newList.add((prevDigit/nextDigit))
-                        restartIndex = i+1
-                    }
-                    '%'->{
-                        newList.add((prevDigit%nextDigit))
                         restartIndex = i+1
                     }
                     else->{
